d63e1ec4520585930626527b78f2d6a9301ef52e
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-01-23 23:00:47 +0530

    Added some code to producer

--- producer/src/main/java/com/demo/producer/controller/KafkaProducer.java
+++ producer/src/main/java/com/demo/producer/controller/KafkaProducer.java
@@ -1,24 +1,35 @@
 package com.demo.producer.controller;
 
 
+import com.demo.producer.model.RiderLocation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
+
+
+//./kafka-console-consumer.sh \
+//        --topic my-topic \
+//        --bootstrap-server kafka:29092 \
+//        --from-beginning \
+//        --property print.partition=true \
+//        --property print.key=true
 
 @RestController
 @RequestMapping("/api")
 public class KafkaProducer {
 
-    private final KafkaTemplate<String,String> kafkaTemplate;
+    private final KafkaTemplate<String,RiderLocation> kafkaTemplate;
 
-    public KafkaProducer(KafkaTemplate<String,String> kafkaTemplate){
+    public KafkaProducer(KafkaTemplate<String,RiderLocation> kafkaTemplate){
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message){
-        kafkaTemplate.send("my-topic", message);
-        return message;
+        RiderLocation riderLocation = new RiderLocation("rider123",28.61,77.23);
+
+        kafkaTemplate.send("my-topic", riderLocation);
+        return "Message sent: "+ riderLocation.getRiderId();
     }
 
 }
